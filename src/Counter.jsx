3e200a055927d2d678307c7f17c1daee1013f427
Jason <holmesjason481@gmail.com>
2024-02-21 14:49:07 -0500

    finished state and event practice

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,21 +2,24 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-
   // update the count state variable when the button is clicked using setCount
   // replace the text inside the h1 tag with the value of count
 
   const increment = () => {
-    //   add code here
+   setCount(count + 1);
   };
 
   const decrement = () => {
-    //   add code here
+   setCount(count-1)
+   if(count<=0){
+   return alert("Can not be Negative")
+
+   }
   };
 
   return (
     <div className="counter">
-      <h1>Counter: "Replace this"</h1>
+      <h1>Counter: {count}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
--- src/RollADice.jsx
+++ src/RollADice.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import Dice from "./Dice";
+import { useState } from "react";
 
 function RollADice() {
+  const [side,setSide] = useState(0)
   // 1. declare a state variable called side and initialize it to 0 - check if useState is imported
 
   // use this function to generate a random number between 0 and 5
   const getRand = () => {
-    return Math.floor(Math.random() * 6);
+    const rand =Math.floor(Math.random() * 6);
+    setSide(rand)
   };
-
   // 2. add onClick function to button to call getRand and set side to the result
 
   // 3. pass side to the prop value of Dice component
 
   return (
     <div className="rolldice">
-      <Dice value={0} />
-      <button>Roll</button>
+      <Dice value={side} />
+      <button onClick={getRand}>Roll</button>
     </div>
   );
 }
